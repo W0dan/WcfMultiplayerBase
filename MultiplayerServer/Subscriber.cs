e433b7d6f6395bf3a97e64d618ac9d47ba2c9a19
W0dan <W0dan.git@gmail.com>
2015-06-18 20:26:06 +0200

    removed unused projects

--- MultiplayerServer/Subscriber.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MultiplayerServer
-{
-    public class Subscriber<TCallback>
-    {
-        public string Token { get; set; }
-        public string Name { get; set; }
-        public TCallback Callback { get; set; }
-    }
-}
